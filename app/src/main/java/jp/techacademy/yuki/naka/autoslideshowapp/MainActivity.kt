c6669574c3b5f7bd8156c02b6b1eda2b282fbb33
Yuki-Naka-Sharing-Study <yukinaka2168@au.com>
2021-01-07 16:29:07 +0900

    課題で行き詰ったので一旦コードレビューをメンターの方にお願いする

--- app/src/main/java/jp/techacademy/yuki/naka/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yuki/naka/autoslideshowapp/MainActivity.kt
@@ -1,16 +1,26 @@
 package jp.techacademy.yuki.naka.autoslideshowapp
 
 import android.Manifest
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.content.ContentUris
 import android.content.pm.PackageManager
 import android.os.Build
-import android.util.Log
+import android.os.Bundle
 import android.provider.MediaStore
-import android.content.ContentUris
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import android.view.View;
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
+
+    private var pager: ViewPager? = null
+
+    private var adapter: FragmentPagerAdapter? = null
+
+    private var currentPage = 0
 
     private val PERMISSIONS_REQUEST_CODE = 100
 
@@ -18,22 +28,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button.setOnClickListener {
-            // Android 6.0以降の場合
+        next_button.setOnClickListener {
+            // パーミッションの許可状態を確認する
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                // パーミッションの許可状態を確認する
                 if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
                     // 許可されている
-                    getContentsInfo()
+                    Log.d("ANDROID", "許可されている")
                 } else {
+                    Log.d("ANDROID", "許可されていない")
                     // 許可されていないので許可ダイアログを表示する
                     requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSIONS_REQUEST_CODE)
                 }
-                // Android 5系以下の場合
-            } else {
-                getContentsInfo()
             }
         }
+
+        back_button.setOnClickListener {
+            // パーミッションの許可状態を確認する
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    // 許可されている
+                    Log.d("ANDROID", "許可されている")
+                } else {
+                    Log.d("ANDROID", "許可されていない")
+                    // 許可されていないので許可ダイアログを表示する
+                    requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSIONS_REQUEST_CODE)
+                }
+            }
+        }
+
+        pager = (ViewPager) findViewById(R.id.pager);
+
+        adapter = new UserInfoViewPagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(adapter);
+        currentPage = 0;
+    }
+
+    fun onClickNext(view: View?) {
+        currentPage++
+        pager.setCurrentItem(currentPage)
+    }
+
+    fun onClickBack(view: View?) {
+        currentPage--
+        pager.setCurrentItem(currentPage)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
