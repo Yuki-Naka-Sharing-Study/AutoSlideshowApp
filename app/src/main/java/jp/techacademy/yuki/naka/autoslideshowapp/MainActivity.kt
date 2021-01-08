11ec50c10a2ebcc1dd11d73455f6b5ec5b2beca6
Yuki-Naka-Sharing-Study <yukinaka2168@au.com>
2021-01-08 21:25:20 +0900

    「cursor」という「クラス」を型として宣言。 （private var cursor: Cursor? = null）

--- app/src/main/java/jp/techacademy/yuki/naka/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yuki/naka/autoslideshowapp/MainActivity.kt
@@ -8,40 +8,30 @@ import android.os.Build
 import android.util.Log
 import android.provider.MediaStore
 import android.content.ContentUris
+import android.database.Cursor
 import kotlinx.android.synthetic.main.activity_main.*
-
 
 class MainActivity : AppCompatActivity() {
 
     private val PERMISSIONS_REQUEST_CODE = 100
-
-    private fun getContentsInfo() {
-        // 画像の情報を取得する
-        val resolver = contentResolver
-        val cursor = resolver.query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
-                null, // 項目（null = 全項目）
-                null, // フィルタ条件（null = フィルタなし）
-                null, // フィルタ用パラメータ
-                null // ソート (nullソートなし）
-        )
-
-        if (cursor!!.moveToFirst()) {
-            do {
-                // indexからIDを取得し、そのIDから画像のURIを取得する
-                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-                val id = cursor.getLong(fieldIndex)
-                val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-
-                Log.d("ANDROID", "URI : " + imageUri.toString())
-            } while (cursor.moveToNext())
-        }
-        cursor.close()
-    }
+    private var cursor: Cursor? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fun getContentsInfo() {
+            // 画像の情報を取得する
+            val resolver = contentResolver
+            this.cursor = cursor
+            resolver.query(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+                    null, // 項目（null = 全項目）
+                    null, // フィルタ条件（null = フィルタなし）
+                    null, // フィルタ用パラメータ
+                    null // ソート (nullソートなし）
+            )
+        }
 
         // Android 6.0以降の場合
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -59,11 +49,31 @@ class MainActivity : AppCompatActivity() {
         }
 
         back_button.setOnClickListener {
+            if (cursor!!.moveToFirst()) {
+                do {
+                    // indexからIDを取得し、そのIDから画像のURIを取得する
+                    val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+                    val id = cursor!!.getLong(fieldIndex)
+                    val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
 
+                    Log.d("ANDROID", "URI : " + imageUri.toString())
+                } while (cursor!!.moveToPrevious())
+            }
+            cursor!!.close()
         }
 
         next_button.setOnClickListener {
+            if (cursor!!.moveToFirst()) {
+                do {
+                    // indexからIDを取得し、そのIDから画像のURIを取得する
+                    val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+                    val id = cursor!!.getLong(fieldIndex)
+                    val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
 
+                    Log.d("ANDROID", "URI : " + imageUri.toString())
+                } while (cursor!!.moveToNext())
+            }
+            cursor!!.close()
         }
     }
 
