0d4a195bacc9185c2a4da81543a7b11481e90144
Yuki-Naka-Sharing-Study <yukinaka2168@au.com>
2021-01-15 15:47:20 +0900

    なぜ「moveToFirst」？ なぜ「do ~ while」？

--- app/src/main/java/jp/techacademy/yuki/naka/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yuki/naka/autoslideshowapp/MainActivity.kt
@@ -15,23 +15,21 @@ class MainActivity : AppCompatActivity() {
 
     private val PERMISSIONS_REQUEST_CODE = 100
     private var cursor: Cursor? = null
+    fun getContentsInfo() {
+        // 画像の情報を取得する
+        val resolver = contentResolver
+        var cursor = resolver.query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+                null, // 項目（null = 全項目）
+                null, // フィルタ条件（null = フィルタなし）
+                null, // フィルタ用パラメータ
+                null // ソート (nullソートなし）
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        fun getContentsInfo() {
-            // 画像の情報を取得する
-            val resolver = contentResolver
-            this.cursor = cursor
-            resolver.query(
-                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
-                    null, // 項目（null = 全項目）
-                    null, // フィルタ条件（null = フィルタなし）
-                    null, // フィルタ用パラメータ
-                    null // ソート (nullソートなし）
-            )
-        }
 
         // Android 6.0以降の場合
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -49,31 +47,14 @@ class MainActivity : AppCompatActivity() {
         }
 
         back_button.setOnClickListener {
-            if (cursor!!.moveToFirst()) {
-                do {
-                    // indexからIDを取得し、そのIDから画像のURIを取得する
-                    val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
-                    val id = cursor!!.getLong(fieldIndex)
-                    val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-
-                    Log.d("ANDROID", "URI : " + imageUri.toString())
-                } while (cursor!!.moveToPrevious())
+            if (cursor!!.moveToPrevious()) {
             }
-            cursor!!.close()
         }
 
-        next_button.setOnClickListener {
-            if (cursor!!.moveToFirst()) {
-                do {
-                    // indexからIDを取得し、そのIDから画像のURIを取得する
-                    val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
-                    val id = cursor!!.getLong(fieldIndex)
-                    val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
 
-                    Log.d("ANDROID", "URI : " + imageUri.toString())
-                } while (cursor!!.moveToNext())
+        next_button.setOnClickListener {
+            if (cursor!!.moveToNext()) {
             }
-            cursor!!.close()
         }
     }
 
@@ -85,5 +66,4 @@ class MainActivity : AppCompatActivity() {
                 }
         }
     }
-
 }
